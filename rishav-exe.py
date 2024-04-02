db54f918aaed60e024a7a8c038e45f4dd8cfe27f
RISHAV-XD <156590973+RISHAV-XD@users.noreply.github.com>
2024-04-02 18:37:30 +0545

    Delete rishav-exe.py

--- rishav-exe.py
+++
@@ -1,7 +0,0 @@
-#-----------------[ IMPORT-MODULE ]-------------------#
-
-def modules():
-	print("\033[1;37m [\u001b[36m•\033[1;37m] CHECKING  UPDATES \033[1;37m")
-	os.system('pkg update -y && pkg upgrade -y')
-	os.system('clear')
-	try:
